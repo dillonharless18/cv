075b5c6cfffd3e658f8d8694ef845e4b82cc9d6d
dillonharless18 <dillonharless@gmail.com>
2022-08-01 19:55:52 -0400

    Overwriting old site with new

--- scripts/fade-to-dark.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $(window).scroll(function () {
-        var currentScrollTop = $(window).scrollTop();
-        $('#blackoverlay').css('opacity',currentScrollTop/$('#blackoverlay').height());
-
-    });
-});
